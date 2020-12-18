5e99fc11cbcbf1f2f8465747500525e5db0d0a82
RobPavl <robert.pavlovsky@gmail.com>
2020-12-18 12:22:53 +0200

    Completed task 2

--- task_2/array.rb
+++ task_2/array.rb
@@ -1,2 +1,18 @@
 class Array
+
+	def rebuild!(&block)
+
+  	  for element in self
+
+  		if element.kind_of?(Array)
+  			element.map!{ |e| yield(e) }                 if block.arity == 1
+  			element.map!{ |e| yield(e, element.length) } if block.arity == 2
+  		else
+  			self.map! { |e|  e == element ? yield(element) : e }              if block.arity == 1
+  			self.map! { |e|  e == element ? yield(element, self.length) : e } if block.arity == 2
+  		end
+
+  	  end
+
+  	end
 end
