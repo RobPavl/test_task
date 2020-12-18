3c5e404f1f8d7656ff3744541ea5b213ab5bc54a
RobPavl <robert.pavlovsky@gmail.com>
2020-12-18 12:24:29 +0200

    Uncompleted task 1

--- task_1/truncate.rb
+++ task_1/truncate.rb
@@ -1,2 +1,11 @@
 module Truncate
+
+# wrote method but don`t know how give here argument 'body' which comes from  test.rb
+  def truncate body, l
+   
+    changed_text = body[0..l[:lenght]].rpartition(" ").first << " ..."
+    
+    changed_text
+  end  
+
 end
